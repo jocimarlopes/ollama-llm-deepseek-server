0b8edd5f2c3ce5028e5cb32d0970424970f4a03c
Jocimar Lopes <lopes.jocimar@gmail.com>
2025-05-24 13:38:21 -0300

    feature/adjusts_model_prompt

--- services/ollama.py
+++ services/ollama.py
@@ -6,7 +6,7 @@ OLLAMA_LOCAL_URL = "http://localhost:11434/api/"  # Ajuste conforme necessário
 def get_payload(prompt, stream=False, model = 'mistral:7b'):
     payload = {
         "model": model,
-        "prompt": 'Responda apenas em português o que os usuários irão pedir a seguir: {}'.format(str(prompt)),
+        "prompt": 'Responda apenas em português o que os usuários irão pedir a seguir, quero que o retorno seja em HTML já formatado como markdown: {}'.format(str(prompt)),
         "stream": stream
     }
     return payload
@@ -27,7 +27,7 @@ def generate_streaming(payload):
                 yield data["response"]
 
 def get_models_ai():
-    response = requests.post(OLLAMA_LOCAL_URL + 'tags')
+    response = requests.get(OLLAMA_LOCAL_URL + 'tags', json={}, stream=False)
     res = response.json()
     lista = []
     for item in res['models']:
